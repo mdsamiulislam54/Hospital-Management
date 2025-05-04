55f354c4413cc3fe3fe997fa46319ca6840857e9
mdsamiulislam54 <samiulm5332@gmail.com>
2025-05-04 11:00:36 +0600

    Sign In page design update

--- src/components/UserAuthentication/Login.jsx
+++ src/components/UserAuthentication/Login.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdOutlineMarkEmailUnread } from "react-icons/md";
 import { FcGoogle } from "react-icons/fc";
 import { IoPhonePortraitOutline } from "react-icons/io5";
 import { Link } from "react-router";
+import { FaRegEye } from "react-icons/fa6";
+import { FaRegEyeSlash } from "react-icons/fa";
+
 
 const Login = () => {
+  const [ispassword, setIsPassword] = useState(false);
   return (
     <div className="h-screen xl:ml-62 px-4 flex flex-col justify-center items-center">
       <form
@@ -14,22 +18,35 @@ const Login = () => {
         <caption className="mb-4 text-xl font-semibold text-white">
           Sign In
         </caption>
+
         <input
-          type="text"
+          type="email"
+          name="email"
+          required
           placeholder="Enter Your Email"
           className="w-full border-b p-2 text-white outline-none"
         />
-        <input
-          type="text"
-          placeholder="Enter Your Email"
-          className="w-full border-b p-2 text-white outline-none"
-        />
+        <span className="relative">
+          <input
+            type={ispassword ? "text" : "password"}
+            name="password"
+            required
+            placeholder="Enter Your Password"
+            className="w-full border-b p-2 text-white outline-none"
+          />
+          <span
+            className="absolute top-3 right-0 text-white"
+            onClick={() => setIsPassword(!ispassword)}
+          >
+            {ispassword ? <FaRegEye /> : <FaRegEyeSlash />}
+          </span>
+        </span>
         <input
           type="submit"
           value="Sign In"
           className="w-full my-4 p-2 text-white btn btn-outline hover:bg-white hover:text-black transition-all duration-500 "
         />
-        <caption>Or</caption>
+        <caption className="text-white text-xl font-bold">Or</caption>
         <div className="flex justify-center items-center flex-col gap-4">
           <button className="btn bg-white text-black border-[#e5e5e5] btn-block">
             <FcGoogle size={24} />
@@ -42,8 +59,11 @@ const Login = () => {
           </button>
         </div>
         <div>
-          <p className="text-center">
-            Don't have a account please ? <Link className="underline text-green-300">create account</Link>
+          <p className="text-center text-white">
+            Don't have an account please ?{" "}
+            <Link to={"/registration"} className="underline text-green-300">
+              create account
+            </Link>
           </p>
         </div>
       </form>
--- src/components/UserAuthentication/Registration.jsx
+++ src/components/UserAuthentication/Registration.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React, { useState } from "react";
+import { MdDriveFileRenameOutline } from "react-icons/md";
+import { Link } from "react-router";
+import { FaRegEye } from "react-icons/fa6";
+import { FaRegEyeSlash } from "react-icons/fa";
 
 const Registration = () => {
+  const [ispassword,setIsPassword] = useState(false)
   return (
-    <div>Registration</div>
-  )
-}
+    <div>
+      <div className="h-screen xl:ml-62 px-4 flex flex-col justify-center items-center">
+        <form
+          action=""
+          className="flex flex-col gap-3 w-5/12 mx-auto shadow-lg p-4 shadow-gray-100"
+        >
+          <caption className="mb-4 text-xl font-semibold text-white">
+            Sign In
+          </caption>
+       
+        <input
+            type="text"
+            name="name"
+            required
+            placeholder="Enter Your Name"
+            className="w-full border-b p-2 text-white outline-none"
+          />
+ 
+          <input
+            type="number"
+            name="phone"
+            required
+            placeholder="Enter Your Phone Number..."
+            className="w-full border-b p-2 text-white outline-none"
+          />
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="Enter Your Email"
+            className="w-full border-b p-2 text-white outline-none"
+          />
+          <span className="relative">
+          <input
+            type={ispassword ? "text":"password"}
+            name="password"
+            required
+            placeholder="Enter Your Password"
+            className="w-full border-b p-2 text-white outline-none"
+          />
+          <span className="absolute top-3 right-0 text-white" onClick={()=>setIsPassword(!ispassword)}>
+            {
+              ispassword ? <FaRegEye />:<FaRegEyeSlash />
+            }
+          </span>
+          </span>
+          <input
+            type="submit"
+            value="Sign Up"
+            className="w-full my-4 p-2 text-white btn btn-outline hover:bg-white hover:text-black transition-all duration-500 "
+          />
+          <caption>Or</caption>
 
-export default Registration
+          <div>
+            <p className="text-center text-white">
+              Already have an account please ?{" "}
+              <Link to={"/login"} className="underline text-green-300">
+                Sign In
+              </Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Registration;
